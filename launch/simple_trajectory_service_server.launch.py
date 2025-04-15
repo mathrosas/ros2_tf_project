edc269cb219b4df4a859004f343b8eb9ee618d9c
mathrosas <mathiasrosas@gmail.com>
2025-04-15 15:27:40 +0000

    Part 2: generate trajectory waypoint as dynamic TF

---
+++ launch/simple_trajectory_service_server.launch.py
@@ -0,0 +1,17 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import ExecuteProcess
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='ros2_tf_project',
+            executable='simple_trajectory_service_server',
+            name='simple_trajectory_service_server',
+            output='screen'
+        ),
+        ExecuteProcess(
+            cmd=['rviz2', '-d', 'install/ros2_tf_project/share/ros2_tf_project/rviz/tb3_burger.rviz'],
+            output='screen'
+        )
+    ])
